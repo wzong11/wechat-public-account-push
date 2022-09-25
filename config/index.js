087dc2cefd405b265f0cd2b347a29b9d7badd7e9
wzong11 <114073160+wzong11@users.noreply.github.com>
2022-09-25 13:12:29 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oK5hU6FAYx9pJ1IbklbxpvpuECwE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "vL-BqmHwMPrMmFkO59tMoqMZphKKMT6YV7CN5oDlfbA",
+      useTemplateId: "BzgOSNQO0j94tqyiwra6_KJkXvMdcFsrdyY-XO3HkC8",
       // 所在省份
       province: "贵州",
       // 所在城市
